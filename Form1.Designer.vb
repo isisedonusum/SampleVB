6c14f49eb4921fe06c5f621a7589a683d799d36e
Mustafa Kerim Yılmaz <mkysoft@gmail.com>
2016-12-12 23:55:28 +0300

    ilk

---
+++ Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.txtURL = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtVKN = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtUser = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(284, 261)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.txtPass)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.txtUser)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.txtVKN)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.txtURL)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(276, 235)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Bilgiler"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.TextBox5)
+        Me.TabPage2.Controls.Add(Me.Button1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(276, 235)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Fatura Listesi"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'txtURL
+        '
+        Me.txtURL.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtURL.Location = New System.Drawing.Point(76, 13)
+        Me.txtURL.Name = "txtURL"
+        Me.txtURL.Size = New System.Drawing.Size(194, 20)
+        Me.txtURL.TabIndex = 0
+        Me.txtURL.Text = "http://musteritestws.isisbilisim.com.tr/services/einvoice/ReportInvoiceList.svc"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "URL"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 42)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "VKN"
+        '
+        'txtVKN
+        '
+        Me.txtVKN.Location = New System.Drawing.Point(76, 39)
+        Me.txtVKN.Name = "txtVKN"
+        Me.txtVKN.Size = New System.Drawing.Size(100, 20)
+        Me.txtVKN.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "User Name"
+        '
+        'txtUser
+        '
+        Me.txtUser.Location = New System.Drawing.Point(76, 65)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.Size = New System.Drawing.Size(100, 20)
+        Me.txtUser.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 94)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(30, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Pass"
+        '
+        'txtPass
+        '
+        Me.txtPass.Location = New System.Drawing.Point(76, 91)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.Size = New System.Drawing.Size(100, 20)
+        Me.txtPass.TabIndex = 6
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(104, 15)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Listele"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.TextBox5.Location = New System.Drawing.Point(3, 44)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(270, 188)
+        Me.TextBox5.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtPass As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtUser As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtVKN As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtURL As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Client As ISISRIL.ReportInvoiceListClient
+        Dim Request As ISISRIL.ContractsOutboundInvoiceListRequest
+        Dim Result As List(Of ISISRIL.OutboxInvoiceHeader)
+        Dim ea As ServiceModel.EndpointAddress
+        Dim cb As ServiceModel.Channels.CustomBinding
+        Dim transport As ServiceModel.Channels.HttpTransportBindingElement
+        Dim textMessageEncoding As ServiceModel.Channels.TextMessageEncodingBindingElement
+
+        TextBox5.Clear()
+
+        ea = New ServiceModel.EndpointAddress(New Uri(txtURL.Text))
+        cb = New ServiceModel.Channels.CustomBinding()
+
+        textMessageEncoding = New ServiceModel.Channels.TextMessageEncodingBindingElement()
+        textMessageEncoding.MessageVersion = ServiceModel.Channels.MessageVersion.Soap11
+        cb.Elements.Add(textMessageEncoding)
+
+        'HTTPS kontrolü
+        If (txtURL.Text.Length > 5 And txtURL.Text.Substring(0, 5).ToLower() = "https") Then
+            transport = New ServiceModel.Channels.HttpsTransportBindingElement()
+        Else
+            transport = New ServiceModel.Channels.HttpTransportBindingElement()
+        End If
+        transport.UseDefaultWebProxy = True
+        transport.BypassProxyOnLocal = True
+
+        If (txtPass.Text <> "") Then
+            transport.AuthenticationScheme = Net.AuthenticationSchemes.Basic
+        End If
+        transport.MaxReceivedMessageSize = Integer.MaxValue
+
+        cb.Elements.Add(transport)
+
+        Client = New ISISRIL.ReportInvoiceListClient(cb, ea)
+        If (txtPass.Text <> "") Then
+            Client.ClientCredentials.UserName.UserName = txtUser.Text
+            Client.ClientCredentials.UserName.Password = txtPass.Text
+        End If
+
+        Using scope As New ServiceModel.OperationContextScope(Client.InnerChannel)
+            Dim httpRequestProperty As ServiceModel.Channels.HttpRequestMessageProperty
+
+            httpRequestProperty = New ServiceModel.Channels.HttpRequestMessageProperty()
+
+            httpRequestProperty.Headers(System.Net.HttpRequestHeader.Authorization) = "Basic " + _
+            Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(Client.ClientCredentials.UserName.UserName + ":" + Client.ClientCredentials.UserName.Password))
+            ServiceModel.OperationContext.Current.OutgoingMessageProperties(ServiceModel.Channels.HttpRequestMessageProperty.Name) = httpRequestProperty
+
+            Request = New ISISRIL.ContractsOutboundInvoiceListRequest()
+            Request.VKN = txtVKN.Text
+            Request.Status = ISISRIL.EnumsOutboundStatus.ALL
+            Request.BeginInvoiceDate = Date.Now.AddDays(-60)
+            Request.EndInvoiceDate = Date.Now
+            Request.BeginRecordDateTime = Date.Now.AddDays(-60)
+            Request.EndRecordDateTime = Date.Now
+            Request.RecordCount = 10
+            Result = Client.OutboundInvoiceListByFilter(Request)
+
+            TextBox5.AppendText(String.Format("{0} adet fatura bulundu.", Result.Count) & Environment.NewLine)
+            For Each invoice In Result
+                TextBox5.AppendText(invoice.ID & Environment.NewLine)
+            Next
+
+        End Using
+
+    End Sub
+End Class
